a29db99e04f702f4cec576e86ba34da0bdfbf2cf
quxiangjun <quxiangjun@cqu.edu.cn>
2020-11-15 17:32:46 +0800

    add version0.2. Complete PE router solve

--- mapping_friendly_CGRA_schedule.py
+++ mapping_friendly_CGRA_schedule.py
@@ -62,11 +62,12 @@ class opterator:
         self.start_op = False  # 是否为起始节点
 
 
-def scheduled(data, npe):
+def scheduled(data, npe, beta):
     """
     调度函数
     @x 输入列表，每个字列表为一个算子的数据
     @npe PEA中的PE数量
+    @beta 目标函数的权重系数
     """
     key_route_length = 0  # 关键路径长度  即时间步从0到key_route_length-1
     op_list = []  # 算子列表
@@ -76,39 +77,43 @@ def scheduled(data, npe):
     # 初始化数据
     for x in data:
         op = opterator()
-        op.id = int(x[0])
-        for i in range(1, 8, 2):
-            if(x[i] != 0):
-                op.children.append( [int(x[i]), int(x[i+1])] )
-                E.append( [int(x[0]), int(x[i]), int(x[i+1])] )
-        op.children = [[x[1], x[2]], [x[3], x[4]], [x[5], x[6]], [x[7], x[8]]]
-        op.earlier_time_step = int(x[9])
-        op.lastest_time_step = int(x[10])
-        if(int(x[11]) == 1):
+        op.id = int(x[0])  # 编号
+        for i in range(1, 8, 2):  # 子节点添加
+            if(x[i] != 0):  # 不为空的子节点
+                op.children.append([int(x[i]), int(x[i+1])])
+                E.append([int(x[0]), int(x[i]), int(x[i+1])])
+        # op.children = [[x[1], x[2]], [x[3], x[4]], [x[5], x[6]], [x[7], x[8]]]
+        op.earlier_time_step = int(x[9])  # 最早时间步
+        op.lastest_time_step = int(x[10])  # 最晚时间步
+        if(int(x[11]) == 1):  # 机动性节点
             op.moves = True
 
         if(int(x[12]) == 0):  # 无父节点
             op.start_op = True
+
         # 计算 可以被插入的路由节点最晚时间步
         # 在找到kernel后计算
 
         for i in op.children:
-            if(i[1] == 1):
+            if(i[1] == 1):  # 有迭代子节点
                 op.generation = True
-        if(op.lastest_time_step > key_route_length):  # 找关键路径的深度
-            key_route_length = op.lastest_time_step
+        if(op.lastest_time_step + 1 > key_route_length):  # 找关键路径的深度
+            key_route_length = op.lastest_time_step + 1  # 关键路径（1~n) = 最大时间步
 
         op_list.append(op)
 
     # 找出kernel
     # 满足基本的启动间隔约束
     # 满足在无路由节点下的pe数量约束 ( 有路由节点的PE数量约束放在ILP中进行 )
+    ii = 2
     for ii in range(2, key_route_length+1):  # 遍历得到最小符合的ii，由于1不可能实现，从2开始
         flag = True  # 符合flag
+
         # 启动间隔约束
         for edge in E:
             if(edge[2] == 0):
                 continue
+
             else:  # 找到启动边
                 t1 = data[edge[0]-1][9]  # 父节点的最早时间步
                 t2 = data[edge[1]-1][9] + ii  # 子节点在下一迭代中的最早时间步
@@ -119,8 +124,9 @@ def scheduled(data, npe):
                     break
         if(flag == False):
             continue
+
         # PE 数量约束
-        count = [0 for i in range(ii)]  # 每一行的pe使用数
+        count = [0 for i in range(ii)]  # 初始化每一行的pe使用数0
         for op in op_list:
             count[op.earlier_time_step % ii] += 1
         for cnt in count:
@@ -129,37 +135,180 @@ def scheduled(data, npe):
         if(flag == False):
             continue
 
+        # 长依赖约束
+        # 检查所有边依赖的 ti %ii == tj%ii
+        for edge in E:
+            ti = data[edge[0]-1][9]
+            tj = data[edge[1]-1][9]
+            if(ti % ii == tj % ii):
+                flag = False
+                break
+        if(flag == False):
+            continue
+        else:
+            break
+
     # 在已知ii的基础上进行路由算子集合与路由节点最晚时间步计算
     for x in data:
-        op = op_list[x[0] - 1 ]
-        max_temp = 0
+        op = op_list[x[0] - 1]  # 取出路由算子
+        max_temp = 0  # 路由最晚时间步
         for child in op.children:
             t2 = 0
-            if(child[1] == 1): # 可迭代算子，要加上ii
-                t2 = data[child[0]-1][10] + ii # 最晚时间步加ii
+            if(child[1] == 1):  # 可迭代算子，要加上ii
+                t2 = data[child[0]-1][10] + ii  # 最晚时间步加ii
             else:
                 t2 = data[child[0]-1][10]
-            if(max_temp < t2-1):
+            if(max_temp < t2-1):  # 最晚路由算子为子节点的最晚时间-1
                 max_temp = t2-1
+        if(len(op.children) == 0):  # 没有子节点的末尾节点，设置路由范围为相同的最早开始步
+            max_temp = op.earlier_time_step
         op.router_lastest_time_step = max_temp
-        op.moves_router = [op.earlier_time_step,op.router_lastest_time_step]  # 算子的路由节点可放置范围 
-        if(max_temp > 0): # 可路由
+        op.moves_router = [op.earlier_time_step,
+                           op.router_lastest_time_step]  # 算子的路由节点可放置范围
+        if(max_temp > 0):  # 可路由
             Router.append(op)
-    
-    # 添加约束
-    prob = LpProblem("CGRA",LpMinimize) # 最小化问题
-    x_var_list = [] # 每个算子x的变量集合，每个字列表
+
+    # 求解
+    # 添加自变量
+    prob = LpProblem("CGRA", LpMinimize)  # 最小化问题
+    x_var_list = []  # 每个算子x的变量集合，每个字列表
     for i in range(len(data)):
         op = op_list[i]
-        tmp = data[i-1]
-        ls1 = []
-        for j in range(op.moves_router[0],op.lastest_time_step+1):
-            ls2 = []
-            for k in range(j,op.moves_router[1]+1):
-                var = pulp.LpVariable( "x_{}_{}_{}".format(i+1,j,k),lowBound=0,cat='Integer')
+        tmp = data[i]
+        ls1 = []  # 每个算子的所有节点集合的 列表
+        # 算子的机动范围
+        for j in range(op.moves_router[0], op.lastest_time_step+1):
+            ls2 = []  # 每个算子的节点列表
+            # 算子的可路由范围
+            for k in range(j, op.moves_router[1]+1):
+                var = pulp.LpVariable("x_{}_{}_{}".format(
+                    i+1, j, k), lowBound=0, cat='Integer')
                 ls2.append(var)
             ls1.append(ls2)
         print(ls1)
+        x_var_list.append(ls1)
+    print()
+
+    # 添加约束
+    constraints = []  # 约束列表
+
+    # 唯一性约束
+    print("唯一性")
+    for i in range(len(x_var_list)):
+        constraints.append(
+            lpSum(x_var_list[i][j][0] for j in range(len(x_var_list[i]))) == 1
+        )
+        print(lpSum(x_var_list[i][j][0]
+                    for j in range(len(x_var_list[i]))) == 1)
+
+    # 排他性
+    print("排他性")
+    for i in range(len(x_var_list)):
+        temp_var_list = x_var_list[i]
+        j = len(temp_var_list)  # 变量的组数
+        k = len(temp_var_list[0])  # 最长的变量组的变量数
+        for m in range(k):
+            if(m <= j-1):
+                constraints.append(
+                    lpSum(temp_var_list[n][m-n] for n in range(m+1)) <= 1
+                )
+                print(lpSum(temp_var_list[n][m-n] for n in range(m+1)) <= 1)
+            else:
+                constraints.append(
+                    lpSum(temp_var_list[n][m-n] for n in range(j)) <= 1
+                )
+                print(lpSum(temp_var_list[n][m-n] for n in range(j)) <= 1)
+
+    # 依赖约束
+    # 父节点调度时间步小于子节点调度时间步
+    print("依赖约束：父节点调度时间步小于子节点调度时间步")
+    for edge in E:
+        father = x_var_list[edge[0]-1]  # 父变量集合
+        kid = x_var_list[edge[1]-1]  # 子变量集合
+        if(edge[2] == 1): # 迭代边
+            for i in range(len(father)):  # 遍历每一个父的调度节点变量
+                constraints.append(
+                    (i + data[edge[0] - 1][9]) * father[i][0] - lpSum((k +
+                                                                    data[edge[1] - 1][9] + ii) * kid[k][0] for k in range(len(kid))) <= -1
+                )
+                print(
+                    (i + data[edge[0] - 1][9]) * father[i][0] - lpSum((k +
+                                                                    data[edge[1] - 1][9] + ii) * kid[k][0] for k in range(len(kid))) <= -1
+                )
+        else:
+            for i in range(len(father)):  # 遍历每一个父的调度节点变量
+                constraints.append(
+                    (i + data[edge[0] - 1][9]) * father[i][0] - lpSum((k +
+                                                                    data[edge[1] - 1][9]) * kid[k][0] for k in range(len(kid))) <= -1
+                )
+                print(
+                    (i + data[edge[0] - 1][9]) * father[i][0] - lpSum((k +
+                                                                    data[edge[1] - 1][9]) * kid[k][0] for k in range(len(kid))) <= -1
+                )
+        
+        
+    # 父算子的所有路由节点都小于子节点中最大时间步
+    print("依赖约束：父算子小于所有子节点中的最大时间步")
+    for i in range(len(op_list)):
+        max_sn = 0  # 子节点中最大时间步
+        op = op_list[i]
+        if(len(op.children) == 0): # 无子节点，跳过约束
+            continue
+        for child in op.children:
+            id = child[0]
+            kid_op = op_list[id-1]
+            if(max_sn < kid_op.lastest_time_step):
+                max_sn = kid_op.lastest_time_step
+        for j in range(len(x_var_list[i])):
+            for k in range(len(x_var_list[i][j])):
+                temp = x_var_list[i][j]
+                constraints.append(
+                    (k + op.earlier_time_step + j) * temp[k] - max_sn <= -1
+                )
+                print(
+                    (k + op.earlier_time_step + j) * temp[k] - max_sn <= -1
+                )
+
+    # PE资源约束
+    print("PE约束")
+    x_var = [[] for i in range(ii)]  # 收集每个ii行积累的算子
+    for i in range(len(x_var_list)):
+        for j in range(len(x_var_list[i])):
+            for k in range(len(x_var_list[i][j])):
+                x_var[(k + (data[i][9]) + j) % ii].append(x_var_list[i][j][k])
+    for ls in x_var:
+        constraints.append(
+            lpSum(ls) <= npe
+        )
+        print(lpSum(ls) <= npe)
+
+    # 添加约束
+    for item in constraints:
+        prob += item
+
+    # 目标方程
+    print("目标方程")
+    # 最大资源使用量
+    Npe = ((lpSum(beta * x_var[i][j])
+            for j in range(len(x_var[i]))) for i in range(len(x_var)))
+    # 路由算子使用量
+    Nins_ls = []
+    for i in range(len(x_var_list)):
+        for j in range(len(x_var_list[i])):
+            for k in range(len(x_var_list[i][j])):
+                if(j != k):
+                    Nins_ls.append(x_var_list[i][j][k])
+    Nins = lpSum(Nins_ls)
+    prob += Npe - Nins
+    print(Npe - Nins)
+
+    prob.solve()
+    # 查看解的状态
+    print("Status:", LpStatus[prob.status])
+    # 查看解
+    for v in prob.variables():
+        print(v.name, "=", v.varValue)
+
+
 if __name__ == '__main__':
-    scheduled(get_data(),16)
-    
+    scheduled(get_data(), 16, 1)
--- test.py
+++
@@ -1,4 +0,0 @@
-from pulp import *
-ls = []
-ls.append(x = LpVariable('x',0,3))
-print(ls)
